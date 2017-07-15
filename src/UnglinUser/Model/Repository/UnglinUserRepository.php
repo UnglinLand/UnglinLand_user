f32db2fff399f6d0e477f375f8949e50292832b0
matthieu88160 <matthieu.vallance@cscfa.fr>
2017-07-15 12:02:01 +0000

    Close #4 Adding UnglinUser repository

---
+++ src/UnglinUser/Model/Repository/UnglinUserRepository.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of UnglinLand-user
+ *
+ * As each files provides by the CSCFA, this file is licensed
+ * under the MIT license.
+ *
+ * PHP version 7.2
+ *
+ * @category Model
+ * @package  UnglinLand-user
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+namespace UnglinLand\UserModule\Model\Repository;
+
+/**
+ * UnglinUser repository
+ *
+ * This interface define the base UnglinUser repository methods
+ *
+ * @category Model
+ * @package  UnglinLand-user
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+interface UnglinUserRepository
+{
+}
